35cf5a255f6657aaf09dbc967e9e87f57b53b327
Pranjul Singh <singhpranjul001@gmail.com>
2024-04-29 14:03:43 +0530

    Initial commit

---
+++ src/main/java/com/practice/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,41 @@
+package com.practice.thymeleafdemo.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    //create a controller method to show the form
+    @RequestMapping("/showForm")
+    public String showForm(){
+        return "helloworld-form";
+    }
+
+    //create a controller method to process the form
+    @RequestMapping("/processForm")
+    public String processForm(){
+        return "helloworld";
+    }
+
+    //need a controller method to read form data and
+    //also add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String upperCase(HttpServletRequest request, Model theModel){
+        //read the request parameters from the HTML form
+        String theName= request.getParameter("studentName");
+
+        //convert to all uppercase
+        theName.toUpperCase();
+
+        //create a message
+        String result="What's up! " +theName;
+
+        //add the message to the model
+        theModel.addAttribute("message",result);
+
+        return "helloworld";
+    }
+}
